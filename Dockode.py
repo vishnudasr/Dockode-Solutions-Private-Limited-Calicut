46bda40ee0c18df159a4420f8491bb515c6b5e81
vishnu das r <144982500+vishnudasr@users.noreply.github.com>
2024-07-11 19:54:48 +0530

    Add files via upload

---
+++ Dockode.py
@@ -0,0 +1,8 @@
+# Internship task -Dockode Solutions Private Limited Calicut
+
+# Input for number of rows and columns
+r = int(input("Enter the number of rows: "))
+col = int(input("Enter the number of columns: "))
+
+for _ in range(r):
+    print('*' * col)
